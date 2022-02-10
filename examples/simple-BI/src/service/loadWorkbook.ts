78262dab729e7fa7d52d1e39b5e9a855f8169926
梓安 <yuanzhian@cai-inc.com>
2022-02-10 14:11:27 +0800

    perf👌:  升级依赖 typescript 版本为 4.4.4，同时解决 ts 报错问题

--- examples/simple-BI/src/service/loadWorkbook.ts
+++ examples/simple-BI/src/service/loadWorkbook.ts
@@ -5,18 +5,21 @@ import { quertyReportList } from '../api/report'
 import workbookDomain from '../domain/workbook'
 import datasetDomain from '../domain/dataset'
 import reportDomain from '../domain/report'
+import { Workbook } from '../domain/workbook'
+import { DatasetItem } from '../domain/dataset'
+import { ReportItem } from '../domain/report'
 
 export default function* loadWorkbook(id: string) {
   // 查询工作簿
-  const workbook = yield effects.call(queryWorkbook, id)
+  const workbook:Workbook = yield effects.call(queryWorkbook, id)
   workbookDomain.entities.workbookList.insert(workbook)
   // 查询引用的数据集
-  const datasetList = yield effects.call(
+  const datasetList:DatasetItem = yield effects.call(
     quertyDatasetList,
     workbook.datasetIdList
   )
   datasetDomain.entities.datasetList.insert(datasetList)
   // 查询管理的报表
-  const reportList = yield effects.call(quertyReportList, workbook.reportIdList)
+  const reportList:ReportItem = yield effects.call(quertyReportList, workbook.reportIdList)
   reportDomain.entities.reportList.insert(reportList)
 }
